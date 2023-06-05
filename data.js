9482232a9c8e92a9429e9a0db2ae2147d521474e
vaibhav bari <barivaibhav82@gmail.com>
2023-06-05 02:52:32 -0700

    css/bootstrap

---
+++ data.js
@@ -0,0 +1,31 @@
+let points = 23;
+console.log(points);
+
+// DATA TYPES VALUES
+
+// 7 DATA TYPES
+
+// NUMBERS 1 2 22 33 76 3.4
+
+// STRING "i am string" "23" also string
+
+// BOOLEAN  TRUE/FALSE
+
+// NULL INTENTIONALLY ABSENCE OF VALUE
+
+// let score = null;
+// score = 34;
+// console.log(score)
+
+
+// STRINGS
+
+console.log("hello coders");
+let firstname ="wow"
+let lastname ="vaibhav"
+console.log(firstname,lastname)
+console.log("hello vaibhav");
+
+// jab bhi direct likhna ho to comma
+//  me hoga agar sab likh rahe to nodoublecomma
+
---
+++ variable.js
@@ -0,0 +1,12 @@
+console.log("Welcome coder")
+
+// LET
+
+let points = 23;
+console.log(points);
+
+// CONSTANT
+
+const value = 10;
+ 
+
